6b42f6195e3257cab02c12b27e5ff5f1bbd7cfc5
sanjaya <kumar.sanjaya@gmail.com>
2022-02-24 10:20:20 +0000

    Hello.java edited online with Bitbucket

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for (int i=1;i<=10;i++){
-			System.out.println("Hello World how are you and  ..."+i);
+			System.out.println("Hello World how are you and it is fine  ..."+i);
 		}
 	}
 }
